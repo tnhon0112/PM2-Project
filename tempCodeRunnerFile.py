e91627cf9afa20d201d749c8208a3db719d4df85
huy nhat <11525017@student.vgu.edu.vn>
2026-04-10 12:43:58 +0700

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-c
+ def draw(self, screen):
+        # Reset the animation loop after enough frames have passed.
+        if self.index >= 9:
+            self.index = 0
+        # Use pygame blit syntax to draw the current bird flap frame.
+        screen.blit(self.image[(self.index // 5) % len(self.image)], self.rect)
+        self.index += 1
